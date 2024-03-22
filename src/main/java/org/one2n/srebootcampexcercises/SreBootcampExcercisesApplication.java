f4e334807dd8cb1dfdbcc2915e529dd166c7a596
Prateek Vijay <prateekvjjpr65@gmail.com>
2024-03-23 01:22:52 +0530

    LFG!

---
+++ src/main/java/org/one2n/srebootcampexcercises/SreBootcampExcercisesApplication.java
@@ -0,0 +1,13 @@
+package org.one2n.srebootcampexcercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SreBootcampExcercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SreBootcampExcercisesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/one2n/srebootcampexcercises/SreBootcampExcercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.one2n.srebootcampexcercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SreBootcampExcercisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
